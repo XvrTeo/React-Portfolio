5a1ae5e3a55adeb2587cac393f45690e304c7e78
Xavier Teo <xvrteo@gmail.com>
2023-05-04 20:28:08 -0700

    Fixed errors

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -36,10 +36,18 @@ function About() {
         <div className="col-6">
           <h1 id="about">About me</h1>
           <h6 style={{ width: "80%", justifyContent: "center" }} id="about">
-            Welcome and thanks for stopping by! My name is Xavier and I am a junior web developer with a
-            background in sales, account management and customer success across multiple industries and markets.
+            As a seasoned professional with years of experience in sales and customer success within the tech industry, I've decided to pursue an exciting career in software development. I am confident that this transition will provide me with endless opportunities for personal and professional growth, and I am determined to make the most of every chance to learn and develop new skills.
+            <br></br><br></br>
+            I am currently completing an intensive software development bootcamp that's giving me the skills and knowledge necessary to thrive in this field. During the program, I honed my skills in programming languages such as Node, MySQL, MongoDB, React, jQuery, REST, and GraphQL. Additionally, I gained hands-on experience in web development, databases, and various development tools and frameworks.
+
+            <br></br><br></br>
+            Throughout my bootcamp, I excelled in various projects and worked collaboratively with my peers to achieve exceptional results. My prior experience in sales and customer success has instilled in me valuable soft skills such as effective communication, collaboration, and problem-solving. As a developer, I'm able to leverage these skills to create effective and efficient solutions that exceed client expectations.
+
+            <br></br><br></br>
+
+            I'm eager to continue learning and growing in the field of software development, and I'm confident that my unique skill set and diverse experience will enable me to make meaningful contributions to any project or team.
             <br></br>
-            <br></br>This page is a perpetual work in progress so please visit again soon!
+
           </h6>
           <br></br>
           <br></br>
