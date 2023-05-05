ebdef1388dbc017cf9395ea92cf3245ab9d31f96
Xavier Teo <xvrteo@gmail.com>
2023-05-04 22:20:16 -0700

    Fixed index.js in Footer

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-The portfolio showcases several sections, including a header, a content section, and a footer. The header displays my name and a navigation menu with sections named About Me, Portfolio, Contact, and Resume. The About Me section provides the user with my background. Within the Portfolio section, users can view various images of applications that I've created, which link to both the deployed application and the corresponding GitHub repository. Users will be able to contact me via email in the Contact section. The Resume section has a list of my proficiencies as well as contain a button to download my resume in PDF format, while the footer provides links to my email, GitHub, and LinkedIn profiles.
+The portfolio showcases several sections, including a header, a content section, and a footer. The header displays my name and a navigation menu with sections named About Me, Portfolio, Contact, and Resume. The About Me section provides the user with my background. Within the Portfolio section, users can view various images of applications that I've created, which link to both the deployed application and the corresponding GitHub repository. Users will be able to contact me via email in the Contact section. The Resume section has a list of my proficiencies as well as contain a button to download my resume in PDF format, while the footer provides links to my email, GitHub, LinkedIn, Stack Overflow profiles.
 
 ## Screenshots
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { IconContext } from "react-icons/lib";
-import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaEnvelope, FaLinkedin, FaStackOverflow } from "react-icons/fa";
 import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
     <Container >
-    <IconContext.Provider value={{ size:"3em"}}>
-    <footer>
-      <Row>
-      <Col md={{ span: 4, offset: 10 }} style={{
-        width: "20rem", marginBottom: "5rem"
-      }}>
-        <a href="mailto:xvrteo@gmail.com">
-          <FaEnvelope />
-        </a>
-        <a href="https://github.com/XvrTeo" target="_blank" rel="noreferrer">
-          <FaGithub />
-        </a>
-        <a href="https://www.linkedin.com/in/xvrteo/" target="_blank" rel="noreferrer">
-          <FaLinkedin />
-        </a>
-        </Col>
-      </Row>
-    </footer>
-    </IconContext.Provider>
+      <IconContext.Provider value={{ size: "3em" }}>
+        <footer>
+          <Row>
+            <Col md={{ span: 4, offset: 10 }} style={{
+              width: "20rem", marginBottom: "5rem"
+            }}>
+              <a href="mailto:xvrteo@gmail.com">
+                <FaEnvelope />
+              </a>
+              <a href="https://github.com/XvrTeo" target="_blank" rel="noreferrer">
+                <FaGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/xvrteo/" target="_blank" rel="noreferrer">
+                <FaLinkedin />
+              </a>
+              <a href="https://stackoverflow.com/users/21822863/" target="_blank" rel="noreferrer">
+                <FaStackOverflow />
+              </a>
+            </Col>
+          </Row>
+        </footer>
+      </IconContext.Provider>
     </Container>
 
   );
