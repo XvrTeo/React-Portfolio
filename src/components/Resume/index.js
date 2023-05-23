d3efb5931b28e31a9eb37b90ca6aa0844c599b0a
Xavier Teo <xvrteo@gmail.com>
2023-05-22 18:15:23 -0700

    Refactored due to errors

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,38 +1,35 @@
 import React from "react";
-import { FaCode, FaUniversity } from "react-icons/fa";
-import { DiJqueryLogo, DiMongodb } from "react-icons/di";
-import { SiMysql, SiHeroku, SiWebpack, SiRedux, SiReactrouter, SiGraphql } from "react-icons/si";
-import { Container, Row, Col } from "react-bootstrap";
+import {
+  FaGitAlt,
+  FaGithub,
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaBootstrap,
+  FaNode,
+  FaReact,
+  FaNpm,
+  FaUniversity,
+  FaCode
+} from "react-icons/fa";
+import {
+  DiJqueryLogo,
+  DiMongodb
+} from "react-icons/di";
+import {
+  SiMysql,
+  SiHeroku,
+  SiWebpack,
+  SiRedux,
+  SiReactrouter,
+  SiGraphql
+} from "react-icons/si";
+import {
+  Container,
+  Row,
+  Col
+} from "react-bootstrap";
 import DevResume from "../../assets/DevResume.pdf";
-
-const toolIcons = [
-  { icon: FaGitAlt, text: "Git" },
-  { icon: SiHeroku, text: "Heroku" },
-  { icon: SiWebpack, text: "Webpack" },
-  { icon: FaCode, text: "Harness" },
-];
-
-const frontEndIcons = [
-  { icon: FaHtml5, text: "HTML" },
-  { icon: FaCss3Alt, text: "CSS" },
-  { icon: FaJsSquare, text: "JavaScript" },
-  { icon: DiJqueryLogo, text: "jQuery" },
-  { icon: FaCode, text: "Responsive design" },
-  { icon: FaReact, text: "React" },
-  { icon: SiReactrouter, text: "React Router" },
-  { icon: SiRedux, text: "Redux" },
-  { icon: FaBootstrap, text: "Bootstrap" },
-];
-
-const backEndIcons = [
-  { icon: FaCode, text: "APIs" },
-  { icon: FaNode, text: "Node" },
-  { icon: FaCode, text: "Express" },
-  { icon: SiMysql, text: "MySQL, Sequelize" },
-  { icon: DiMongodb, text: "MongoDB, Mongoose" },
-  { icon: FaCode, text: "REST" },
-  { icon: SiGraphql, text: "GraphQL" },
-];
 
 function Proficiencies() {
   return (
@@ -40,13 +37,12 @@ function Proficiencies() {
       <h1 className="text-center mb-5">Proficiencies</h1>
       <Row>
         <Col xs={12} md={4} className="mb-4">
-          <h3>Tools:</h3>
+          <h3><FaGithub /> Tools:</h3>
           <ul className="list-unstyled">
-            {toolIcons.map(({ icon: Icon, text }) => (
-              <li key={text}>
-                <Icon /> {text}
-              </li>
-            ))}
+            <li><FaGitAlt /> Git</li>
+            <li><SiHeroku /> Heroku</li>
+            <li><SiWebpack /> Webpack</li>
+            <li><FaCode /> Harness</li>
           </ul>
           <a
             href={DevResume}
@@ -58,23 +54,27 @@ function Proficiencies() {
           </a>
         </Col>
         <Col xs={12} md={4} className="mb-4">
-          <h3>Front-end:</h3>
+          <h3><FaHtml5 /> Front-end:</h3>
           <ul className="list-unstyled">
-            {frontEndIcons.map(({ icon: Icon, text }) => (
-              <li key={text}>
-                <Icon /> {text}
-              </li>
-            ))}
+            <li><FaHtml5 /> HTML</li>
+            <li><FaCss3Alt /> CSS</li>
+            <li><FaJsSquare /> JavaScript</li>
+            <li><DiJqueryLogo /> jQuery</li>
+            <li><FaCode /> Responsive design</li>
+            <li><FaReact /> <SiReactrouter /> <SiRedux /> React </li>
+            <li><FaBootstrap /> Bootstrap</li>
           </ul>
         </Col>
         <Col xs={12} md={4} className="mb-4">
-          <h3>Back-end:</h3>
+          <h3><FaNode /> Back-end:</h3>
           <ul className="list-unstyled">
-            {backEndIcons.map(({ icon: Icon, text }) => (
-              <li key={text}>
-                <Icon /> {text}
-              </li>
-            ))}
+            <li><FaCode /> APIs</li>
+            <li><FaNode /> <FaNpm /> Node</li>
+            <li><FaCode /> Express</li>
+            <li><SiMysql /> MySQL, Sequelize</li>
+            <li><DiMongodb /> MongoDB, Mongoose</li>
+            <li><FaCode /> REST</li>
+            <li><SiGraphql /> GraphQL</li>
           </ul>
         </Col>
       </Row>
@@ -93,7 +93,8 @@ function Education() {
               <FaUniversity /> Bachelor of Arts in Mass Communications, University of California, Berkeley
             </li>
             <li>
-              <FaCode />{"\n"}
+              <FaCode />{' '}
+              <br />
               Full Stack Coding Bootcamp, UC Berkeley Extension, May 2023
             </li>
           </ul>
@@ -108,10 +109,9 @@ function Resume() {
     <>
       <Proficiencies />
       <Education />
-      <div className="fixed-bottom text-end pe-3 pb-3">
-        <a href="https://sbr.com.sg/media-marketing/commentary/untapping-social-media-potential" target="_blank" rel="noreferrer">
-          "Untapping the social media potential" in the Singapore Business Review
-        </a>
+      <div className="fixed-bottom text-end pe-3 pb
+-3">
+        <a href="https://sbr.com.sg/media-marketing/commentary/untapping-social-media-potential" target="_blank" rel="noreferrer">"Untapping the social media potential" in the Singapore Business Review</a>
       </div>
     </>
   );
