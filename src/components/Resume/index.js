dd961330874f5e5857db20fea892ef67d9b3ba23
Xavier Teo <xvrteo@gmail.com>
2023-05-22 18:20:09 -0700

    Refactored again due to layout issues

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -92,10 +92,9 @@ function Education() {
             <li>
               <FaUniversity /> Bachelor of Arts in Mass Communications, University of California, Berkeley
             </li>
+            <br />
             <li>
-              <FaCode />{' '}
-              <br />
-              Full Stack Coding Bootcamp, UC Berkeley Extension, May 2023
+              <FaCode /> Full Stack Coding Bootcamp, UC Berkeley Extension, May 2023
             </li>
           </ul>
         </Col>
